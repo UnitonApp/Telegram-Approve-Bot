4577af4c75b2b908244a4fd51b2c864272fe6bb2
Skat100500 <slava2006tukin@mail.ru>
2023-08-04 16:45:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import telebot as telebot
+from pyTONPublicAPI import pyTONPublicAPI
+from telebot.apihelper import approve_chat_join_request
+
+BOT_TOKEN = 'YourBotToken'
+chat_id = 00000000000
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def test(message):
+    user_id = message.from_user.id
+    address = message.text
+    client = pyTONPublicAPI()
+    is_premium = message.from_user.is_premium
+    try:
+        balanse = client.get_address_balance(address=address)
+        if balanse > 0 or is_premium:
+            bot.send_message(user_id, 'Твоя заявка одобрена')
+            approve_chat_join_request(token=BOT_TOKEN, chat_id=chat_id, user_id=user_id)
+        else:
+            msg = bot.send_message(user_id, 'На твоем аккаунте нет  Telegram Premium,'
+                                            'а на кошельке нет нужной NFT. повторите '
+                                            'попытку после покупки Telegram Premium '
+                                            'или пополнения кошелька')
+            bot.register_next_step_handler(msg, test)
+    except Exception:
+        msg = bot.send_message(user_id, 'Неверный адрес кошелька. Повторите попытку')
+        bot.register_next_step_handler(msg, test)
+
+
+@bot.chat_join_request_handler()
+def lalala(message: telebot.types.ChatJoinRequest):
+    msg = bot.send_message(message.user_chat_id,
+                           'Привет, ты подал заявку на закрытый чат. Чтобы тебя принять, '
+                           'привяжи свой TON кошелек к боту. '
+                           'Этого будет достаточно, чтобы твоя заявка была одобрена. '
+                           'Если на нём будет найдена минимум 1 NFT из нижеуказанных '
+                           'коллекций, либо активируй Telegram Premium')
+    bot.register_next_step_handler(msg, test)
+
+
+bot.infinity_polling(allowed_updates=telebot.util.update_types)
